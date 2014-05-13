27a47fc2776d12e0249b510dfb60881d48ea9819
Raffaele Rasini <raffaele.rasini@gmail.com>
2014-05-13 15:22:07 +0200

    Shaved 7s in execution time

--- 21.js
+++ 21.js
@@ -1,32 +1,36 @@
 function getFactors(n) {
   var result = [1];
 
-  for (var bottom = 2, top = n; bottom < top; bottom++) {
+  for (var bottom = 2, top = Math.floor(Math.sqrt(n)); bottom < top; bottom++) {
     for (var i = top; i > bottom; i--) {
-        if (bottom * i == n) {
-          top = i;
-          result.push(bottom, top);
-          break;
-        }
+      if (bottom * i == n) {
+        top = i;
+        result.push(bottom, top);
+        break;
+      }
     }
   }
 
   return result.reduce(function(a, b) { return a + b});
 }
 
-var fact = [], tmp_1, tmp_2;
+var sums = {}, fact = [], tmp_1, tmp_2;
 
-for (var j = 0; j < 10000; ++j) {
-  if (j !== fact[fact.length - 1]) {
-    tmp_1 = getFactors(j);      // Sum of factor fro current number
-    tmp_2 = getFactors(tmp_1);  // Get factor of sum of factor
+for (var i = 1; i <= 10000; ++i) {
+  sums[i] = getFactors(i);
+}
+
+for (var i = 0; i < 10000; ++i) {
+  if (i !== fact[fact.length - 1]) {
+    tmp_1 = sums[i];      // Sum of factor for current number
+    tmp_2 = sums[tmp_1];  // Get factor of sum of factor
 
     // [1] Check if is not a prime number
     //  |             [2] Check s is not the same number (es: 6 or 28)
     //  |              |             [3] Check if is an amicable number
     //  |              |              |
-    if (tmp_1 !== 1 && j !== tmp_1 && j === tmp_2) {
-      fact.push(j, tmp_1);
+    if (tmp_1 !== 1 && i !== tmp_1 && i === tmp_2) {
+      fact.push(i, tmp_1);
     }
   }
 
@@ -34,4 +38,4 @@ for (var j = 0; j < 10000; ++j) {
 
 console.log(fact.reduce(function(a, b) {return a + b }));
 
-// Execution time: 46618ms  <- Yep, I need to revisit this one
+// Execution time: 39512ms  <- Yep, I need to revisit this one
